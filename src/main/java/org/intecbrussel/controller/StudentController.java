fc2199a9e9e2d3362f9ef9546b5505a90ece5a5f
Shayma1080 <hajji.chaima@outlook.fr>
2025-11-25 11:31:48 +0100

    update

--- src/main/java/org/intecbrussel/controller/StudentController.java
+++ src/main/java/org/intecbrussel/controller/StudentController.java
@@ -1,4 +1,35 @@
 package org.intecbrussel.controller;
 
+import org.intecbrussel.model.StudentModel;
+import org.intecbrussel.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+    // met constructor ook mogelijk maar eentje kiezen
+
+    @GetMapping
+    public List<StudentModel> getStudents() {
+        return studentService.getStudent();
+    }
+
+    @PostMapping("/savestudent")
+    public void registerNewStudent(@RequestBody StudentModel student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path =  "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+
+
 }
--- src/main/java/org/intecbrussel/service/StudentService.java
+++ src/main/java/org/intecbrussel/service/StudentService.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service // Geeft aan dat deze klasse de business logic bevat. Spring maakt auto een Bean van deze klass (Dependency injection
+@Service // Geeft aan dat deze klasse de business logic bevat. Spring maakt auto een Bean van deze klass (Dependency injection mogelijk)
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    @Autowired // zorgt dat Spring auto de juiste Bean kiest. Dit is de Dependency injection: Service heeft Repository nodig om te
+    @Autowired // zorgt dat Spring auto de juiste Bean kiest. Dit is de Dependency injection: Service heeft Repository nodig om te werken.
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
         //Best practice: contructor injection (zoals hier)
@@ -22,4 +23,21 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public void addNewStudent(StudentModel student) {
+        Optional<StudentModel> optionalStudent = studentRepository.findStudentModelByEmail(student.getEmail());
+        if(optionalStudent.isPresent()) {
+            throw new IllegalStateException("Student already exists");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("Student id" + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
 }
+//de methoden komt van jpa repository
